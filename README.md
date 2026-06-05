8ff4e1a99a954e7613901e8685a9b46009636d11
MAKARYAN Raphael <contact@raphaelmakaryan.fr>
2026-06-05 10:39:49 +0200

    Commencement sur l'exercice du voyageur de commerce

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
-# algo_cnita
-
-# Structures
-
-Les exercices concernant le cours d'algo d'avancée sont structurés en deux branches : 
-- "js" : exercices en JavaScript
-- "python" : exercices en Python
+# Branche Voyageur de commerce
---
+++ index.py
@@ -0,0 +1,53 @@
+import math
+
+import folium
+
+m = folium.Map(location=(45.5236, -122.6750))
+allMarkers = [
+    # Position, Texte en hover, Nom au clique, icone
+    [[45.3288, -121.6625], "Click moi dessus la", "Mt. Hood Meadows", "cloud"],
+    [[45.3311, -121.7113], "Click moi dessus aller", "Timberline Lodge", "cloud"]
+]
+
+
+def createMarkers(allMarkers, m):
+    trail_coordinates = []
+    allCoordinates = []
+    for marker in allMarkers:
+        coordinatesForTrail = (marker[0][0], marker[0][1])
+        trail_coordinates.append(coordinatesForTrail)
+        allCoordinates.append(marker[0])
+        folium.Marker(
+            location=marker[0],
+            tooltip=marker[1],
+            popup=marker[2],
+            icon=folium.Icon(icon=marker[3]),
+        ).add_to(m)
+    createLine(trail_coordinates, m)
+    have_distance(allCoordinates)
+
+
+def createLine(allMarkersCoordinates, m):
+    folium.PolyLine(allMarkersCoordinates, tooltip="Coast").add_to(m)
+
+
+def have_distance(allMarkersCoordinates):
+    R = 6371.0
+    lat1_rad, lon1_rad = math.radians(allMarkersCoordinates[0][0]), math.radians(allMarkersCoordinates[0][1])
+    lat2_rad, lon2_rad = math.radians(allMarkersCoordinates[1][0]), math.radians(allMarkersCoordinates[1][1])
+    dlat = lat2_rad - lat1_rad
+    dlon = lon2_rad - lon1_rad
+    a = math.sin(dlat / 2) ** 2 + math.cos(lat1_rad) * math.cos(lat2_rad) * math.sin(dlon / 2) ** 2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    total = (R * c)
+    print(f"Distance entre les deux points : {total:.2f} km")
+
+
+def main():
+    createMarkers(allMarkers, m)
+    m.save("index.html")
+    print("Fin de la création de la map.")
+
+
+if __name__ == "__main__":
+    main()
